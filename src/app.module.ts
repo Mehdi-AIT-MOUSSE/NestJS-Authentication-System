2185c15cf059f6b42e4a46053e88af329d8a3daf
Mehdi <mehdiaitmousse3@gmail.com>
2026-09-12 02:37:55 +0000

    add tasks module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,7 @@ import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
 import { JwtModule } from '@nestjs/jwt';
-import { RolesGuard } from './common/guards/roles.guard';
-
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [
@@ -24,11 +23,11 @@ import { RolesGuard } from './common/guards/roles.guard';
     JwtModule.register({ global: true }),
     UsersModule,
     AuthModule,
+    TasksModule,
   ],
   providers: [
     { provide: APP_GUARD, useClass: ThrottlerGuard },
     { provide: APP_GUARD, useClass: JwtAuthGuard },
-    { provide: APP_GUARD, useClass: RolesGuard },
   ],
 })
 export class AppModule {}
