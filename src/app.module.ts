634513c8cff24dc9dbd1bfaefcb810287442a15e
Mehdi <mehdiaitmousse3@gmail.com>
2026-09-12 01:39:10 +0000

    jwt auth guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,34 @@
 import { Module } from '@nestjs/common';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { ConfigModule } from '@nestjs/config';
-import { UsersService } from './users/users.service';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
+import { JwtModule } from '@nestjs/jwt';
+import { RolesGuard } from './common/guards/roles.guard';
+
 
 @Module({
   imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 20,
+      },
+    ]),
     ConfigModule.forRoot({
       isGlobal: true,
       expandVariables: true,
     }),
-    UsersModule
+    JwtModule.register({ global: true }),
+    UsersModule,
+    AuthModule,
   ],
-  providers: [UsersService],
+  providers: [
+    { provide: APP_GUARD, useClass: ThrottlerGuard },
+    { provide: APP_GUARD, useClass: JwtAuthGuard },
+    { provide: APP_GUARD, useClass: RolesGuard },
+  ],
 })
 export class AppModule {}
